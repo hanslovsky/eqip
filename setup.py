9c203e16b775968dcd7e2addd8acf4000c279a5e
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-12-19 13:18:32 -0500

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from os import path
+
+setup(
+    name='qip',
+    version='0.1.0dev',
+    author='Philipp Hanslovsky',
+    author_email='hanslovskyp@janelia.hhmi.org',
+    description='',
+    url='https://github.com/hanslovsky/qip',
+    install_requires=['gunpowder', 'scikit-image', 'numpy']
+)
