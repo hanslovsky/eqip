821db9ebb7e0aa917ed025aa0d53ae8ca56b41fb
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-01-19 10:43:56 -0500

    Improve version syntax

--- eqip/version.py
+++ eqip/version.py
@@ -1,1 +1,11 @@
-__version__ = '0.2.1dev'
+import sys
+
+__major__   = 0
+__minor__   = 2
+__patch__   = 1
+__tag__     = 'dev'
+# Unfortunately, < 3.5 does not support literal format strings -- too bad!
+# if sys.version_info[0] >= 3 and sys.version_info[1] >= 5:
+#     __version__ = f'{__major__}.{__minor__}.{__patch__}{__tag__}'
+# else:
+__version__ = '{}.{}.{}{}'.format(__major__, __minor__, __patch__, __tag__)
