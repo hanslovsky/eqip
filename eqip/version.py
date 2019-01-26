6bac2a250e17563b308f955029634c233def1948
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-01-26 18:06:55 -0500

    Bump to next development cycle

--- eqip/version.py
+++ eqip/version.py
@@ -2,8 +2,8 @@ import sys
 
 __major__   = 0
 __minor__   = 3
-__patch__   = 0
-__tag__     = ''
+__patch__   = 1
+__tag__     = 'dev'
 # Unfortunately, < 3.5 does not support literal format strings -- too bad!
 # if sys.version_info[0] >= 3 and sys.version_info[1] >= 5:
 #     __version__ = f'{__major__}.{__minor__}.{__patch__}{__tag__}'
